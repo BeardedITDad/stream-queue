c69c2e80d8dac5e410ee1017fe6520178eb76b31
BeardedITDad <84863941+BeardedITDad@users.noreply.github.com>
2026-02-20 09:02:37 -0800

    Update background color and margin in page layout

--- app/page.tsx
+++ app/page.tsx
@@ -94,9 +94,9 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-900 text-white p-10 font-sans flex flex-col justify-between">
+    <div className="min-h-screen bg-[#292e3d] text-white p-10 font-sans flex flex-col justify-between">
       {/* --- INSERT LOGO HERE --- */}
-    <div className="flex justify-center w-full mb-0">
+    <div className="flex justify-center w-full -mb-6">
       <img 
         src="/IT CAREER (1024 x 500 px).png" 
         alt="IT Career Accelerator" 
